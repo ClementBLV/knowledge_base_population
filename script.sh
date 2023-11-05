5df591b663e428c9f80c9de08b6e1ba6cbb1a1b7
Clement BELIVEAU <c20beliv@sl-tp-br-501.imta.fr>
2023-11-05 16:03:49 +0100

    Bug Fix: Python3

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ TEST=$ROOT"source/test"
 VALID=$ROOT"source/valid"
 cd src
 # preprocess the raw dataset
-python data_generator.py --train-path $TRAIN".txt" --valid-path $VALID".txt" --test-path $TEST".txt"
+python3 data_generator.py --train-path $TRAIN".txt" --valid-path $VALID".txt" --test-path $TEST".txt"
 
 echo "******* TRAIN *******"
 # split the train set
@@ -13,12 +13,12 @@ split_values=(5 10 20)
 for SPLIT in "${split_values[@]}"; do
         # Split train
         echo "$SPLIT"
-	!(python split.py --input_file $TRAIN".json" --percentage $SPLIT --output_file $ROOT"train_"$SPLIT".json")
+	!(python3 split.py --input_file $TRAIN".json" --percentage $SPLIT --output_file $ROOT"train_"$SPLIT".json")
 	# convert to mnli format
 	!(python wn2mnli.py --input_file $ROOT"train_"$SPLIT".json" --output_file $ROOT"train_"$SPLIT".mnli.json")
 done
 # convert to NLI format
 echo "******* TEST *******"
-python wn2mnli.py --input_file $TEST".json" --output_file $ROOT"test.mnli.json"
+python3 wn2mnli.py --input_file $TEST".json" --output_file $ROOT"test.mnli.json"
 echo "******* VALID *******"
-python wn2mnli.py --input_file $VALID".json" --output_file $ROOT"valid.mnli.json"
+python3 wn2mnli.py --input_file $VALID".json" --output_file $ROOT"valid.mnli.json"
