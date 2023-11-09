0015ecd92419c57e2b8cbc08744711c5766c2739
Clement BELIVEAU <c20beliv@sl-tp-br-501.imta.fr>
2023-11-09 15:51:50 +0100

    Feature : add the possibility to choose the file to generate

--- script.sh
+++ script.sh
@@ -4,22 +4,39 @@ ROOT=$BASE"/data/WN18RR/"
 TRAIN=$ROOT"source/train"
 TEST=$ROOT"source/test"
 VALID=$ROOT"source/valid"
+
+# whoch one to do
+TRAIN_BOOL=true
+TEST_BOOL=false
+VALID_BOOL=false
+
 cd src
 # preprocess the raw dataset
 python3 data_generator.py --train-path $TRAIN".txt" --valid-path $VALID".txt" --test-path $TEST".txt"
 
+
+
 echo "******* TRAIN *******"
 # split the train set
 #split_values=(5 10 20)
-for SPLIT in 5 10 20; do
-        # Split train
-        echo "$SPLIT"
-	!(python3 split.py --input_file $TRAIN".json" --percentage $SPLIT --output_file $ROOT"train_"$SPLIT".json")
-	# convert to mnli format
-	!(python wn2mnli.py --input_file $ROOT"train_"$SPLIT".json" --output_file $ROOT"train_"$SPLIT".mnli.json")
-done
+
+if $TRAIN_BOOL; then
+	for SPLIT in 1; do
+			# Split train
+			echo "$SPLIT"
+		!(python3 split.py --input_file $TRAIN".json" --percentage $SPLIT --output_file $ROOT"train_"$SPLIT".json")
+		# convert to mnli format
+		!(python3 wn2mnli.py --input_file $ROOT"train_"$SPLIT".json" --output_file $ROOT"train_"$SPLIT".mnli.json")
+	done
+fi
+
 # convert to NLI format
-echo "******* TEST *******"
-python3 wn2mnli.py --input_file $TEST".json" --output_file $ROOT"test.mnli.json"
-echo "******* VALID *******"
-python3 wn2mnli.py --input_file $VALID".json" --output_file $ROOT"valid.mnli.json"
+if $TEST_BOOL; then
+	echo "******* TEST *******"
+	python3 wn2mnli.py --input_file $TEST".json" --output_file $ROOT"test.mnli.json"
+fi
+
+if $VALID_BOOL; then
+	echo "******* VALID *******"
+	python3 wn2mnli.py --input_file $VALID".json" --output_file $ROOT"valid.mnli.json"
+fi
