9e4f27993fe3980260d8898c5b86cb5cf7a0a4dc
Clement BELIVEAU <c20beliv@sl-tp-br-501.imta.fr>
2023-11-18 10:18:57 +0100

    Feature : add the homogeneisation

--- src/split.py
+++ src/split.py
@@ -7,8 +7,10 @@ parser = ArgumentParser()
 
 parser.add_argument("--input_file", type=str)
 parser.add_argument("--output_file", type=str)
+parser.add_argument("--bias", type=bool,  help="If true the output will be biased else it will not be")
 parser.add_argument("--percentage", type=int, default=5, 
                     help= "The percentage 5 for 5% , 10 for 10%, correponding to the number of lines kept")
+parser.add_argument("--threshold_effectif", type=int,  help="Minimal number of relation")
 
 args = parser.parse_args()
 
@@ -26,28 +28,84 @@ class REInputFeatures:
     context: str
     relation: str = None
 
+if args.threshold_effectif is None : 
+    # random pickup 
+    with open(args.input_file, "rt") as f:
+        mnli_data = []
+        stats = []
+        lines = json.load(f)
+        print("Number of inputs : " , len(lines))
+        print("Number of ouputs : " , round(len(lines)*args.percentage/100))
+                            
+        for line in random.choices(lines, k=round(len(lines)*args.percentage/100)):
 
-# random pickup 
-with open(args.input_file, "rt") as f:
-    mnli_data = []
-    stats = []
-    lines = json.load(f)
-    print("Number of inputs : " , len(lines))
-    print("Number of ouputs : " , round(len(lines)*args.percentage/100))
-                          
-    for line in random.choices(lines, k=round(len(lines)*args.percentage/100)):
+            mnli_data.append(
+                REInputFeatures(
+                    premise=  line["premise"],
+                    subj =  line["subj"],
+                    obj =  line["obj"],
+                    context   =  line["context"],
+                    relation =  line["relation"],
+                    )
+            )
+            
+        print("Real percentage : " , len(mnli_data)/len(lines))
 
-        mnli_data.append(
-            REInputFeatures(
-                premise=  line["premise"],
-                subj =  line["subj"],
-                obj =  line["obj"],
-                context   =  line["context"],
-                relation =  line["relation"],
+else : 
+    # homogeneous random picking throuout all the relations 
+    with open(args.input_file, "rt") as f:
+        mnli_data = []
+        stats = []
+        lines = json.load(f)
+        print("Number of inputs : " , len(lines))
+        print("Number of ouputs : " , round(len(lines)*args.percentage/100))
+        relation2data = {}            
+        for line in lines:
+            # accumulate all the elements for each relations 
+            if line["relation"] in relation2data: 
+                relation2data[line["relation"]].append(
+                    REInputFeatures(
+                        premise=  line["premise"],
+                        subj =  line["subj"],
+                        obj =  line["obj"],
+                        context   =  line["context"],
+                        relation =  line["relation"],
+                        )
                 )
-        )
-        
-    print("Real percentage : " , len(mnli_data)/len(lines))
+            else : 
+                relation2data[line["relation"]] = [
+                    REInputFeatures(
+                        premise=  line["premise"],
+                        subj =  line["subj"],
+                        obj =  line["obj"],
+                        context   =  line["context"],
+                        relation =  line["relation"],
+                        )
+                ]
+        # compute the effective of each data
+        r2eff = {}
+        for key, value in relation2data.items():
+            num_elements = len(value)
+            r2eff[key] = num_elements
+
+        # pich up the relation according to the number of relation   
+        for rel , effective in r2eff.items():
+            if effective == args.threshold_effectif : 
+                mnli_data.extend(relation2data[rel]) # add all the relation 
+            if effective > args.threshold_effectif:
+            # if more data choose randomnly as much elt as the minimal bound 
+                mnli_data.extend(random.choices(relation2data[rel], k=args.threshold_effectif))
+            # else the class is disgad
+        # shuffle the final output
+        if args.percentage ==7:
+            # 0 means all so just suffle  
+            random.shuffle(mnli_data)
+
+        else : 
+            mnli_data = random.choices(lines, k=round(len(mnli_data)*args.percentage/100))
+print(len(mnli_data))
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")        
+
 
 # save
 json.dump([data.__dict__ for data in mnli_data], open(args.output_file, "w",encoding='utf-8'), indent=4)
