63963e3067a60622683822c55f4684ff493bfbbd
Clement BELIVEAU <c20beliv@sl-tp-br-511.imta.fr>
2023-12-01 11:28:48 +0100

    Feature : add loop for the split and the vertion

--- script_eval.sh
+++ script_eval.sh
@@ -4,15 +4,22 @@ BASE=$(pwd)
 cd src
 echo "******* EVAL *******"
 
-# "MoritzLaurer/DeBERTa-v3-base-mnli-fever-docnli-ling-2c"
-MODEL=#"MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
-SPLIT=1
-SAVE_NAME="train_"$SPLIT"_train_dbl"
+#"/users/local/c20beliv/model/$SAVE_NAME/MNLI" \
+#SAVE_NAME="zero_shot_trained_large_v"
 
-python3 eval.py \
-    --input_file $BASE"/data/WN18RR/valid_eval.json" \
-    --output_file "eval" \
-    --model "/users/local/c20beliv/model/$SAVE_NAME/MNLI" \
-    --name "train_"$SPLIT"_deberta_MoritzLaurer_base" \
-    --source_model "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7" \
 
+MODEL="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli"
+#MODEL="microsoft/deberta-v3-base"
+for SPLIT in 20; do
+
+    SAVE_NAME="trained_derbertabase_biased_split"$SPLIT"_v"
+
+    for i in 3; do
+        python3 eval.py \
+            --input_file $BASE"/data/WN18RR/test_eval.json" \
+            --output_file "eval" \
+            --model "/users/local/c20beliv/model/$SAVE_NAME$i/MNLI/" \
+            --name $SAVE_NAME"$i" \
+            --source_model $MODEL 
+    done
+done
