562aadd6fbc4a0b1c94c70a4313ef56c2d914814
ClementBLV <clement.beliveau@imt-atlantique.net>
2024-05-26 14:38:07 +0200

    Feature : add the MNLI datagenerator for freebase + a global generate_nli_data which call either wn one or fb one depending of the task

--- src/data_generator.py
+++ src/data_generator.py
@@ -40,17 +40,27 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+### Global fonction
+def generate_nli_data(path:str, dataset:str=args.task):
+    assert str(type(dataset))=='str', "Must presise the dataset either 'wordnet', 'wn', 'wn18rr' or 'freebase', 'fb', 'fb15k237' in a string"
+    if dataset.lower() in ['wordnet', 'wn', 'wn18rr']: 
+        return generate_nli_data_wordnet(path)
+    if dataset.lower() in ['freebase', 'fb', 'fb15k237']: 
+        return generate_nli_data_freebase(path)
 
-def generate_nli_data(path):
+
+### case of wordnet
+def generate_nli_data_wordnet(path):
     head2tail_dict = preprocess.preprocess_wn18rr(path)
     head2tail_df = pd.DataFrame(head2tail_dict)
     hypos2premises = []
     for index in range(0, len(head2tail_df)):
         triplet = head2tail_df.iloc[index]
         hypo2premise = {}
-        s_head = get_synset(triplet["head"], triplet["head_id"])
+        # get the synset object from NLTK using the id to have more info like lemmas 
+        s_head = get_synset(triplet["head"], triplet["head_id"]) 
         s_tail = get_synset(triplet["tail"], triplet["tail_id"])
-
+        # process the retreived lemmas
         l_head = " or ".join(
             [lemma.name().replace("_", " ").strip() for lemma in s_head.lemmas()][0:2]
         )
@@ -58,6 +68,7 @@ def generate_nli_data(path):
             [lemma.name().replace("_", " ").strip() for lemma in s_tail.lemmas()][0:2]
         )
 
+        # dictionnary contruction 
         hypo2premise["head_id"] = triplet["head_id"]
         hypo2premise["tail_id"] = triplet["tail_id"]
         hypo2premise["context"] = (
@@ -108,14 +119,48 @@ def get_synset(name, id):
         print("+" + name)
 
 
+### case of freebase
+def generate_nli_data_freebase(path):
+    head2tail_dict , desciptions = preprocess.preprocess_fb15k237(path)
+    all_id = list(desciptions.keys())
+    head2tail_df = pd.DataFrame(head2tail_dict)
+    hypos2premises = []
+    for index in range(0, len(head2tail_df)):
+        triplet = head2tail_df.iloc[index]
+        # remove the case with no description availaible
+        if triplet["head_id"] in all_id and triplet["tail_id"] in all_id: 
+            hypo2premise = {}
+            hypo2premise["head_id"] = triplet["head_id"]
+            hypo2premise["tail_id"] = triplet["tail_id"]
+            hypo2premise["context"] = (
+                desciptions[triplet["head_id"]] + ". " + desciptions[triplet["tail_id"]].replace("@en", '')
+            ) # context formed with the definitions
+            hypo2premise["premise"] = (
+                triplet["head"] + " <" + str(triplet["relation"]) + "> " + triplet["tail"]
+            )
+            hypo2premise["subj"] = triplet["tail"]
+            hypo2premise["obj"] = triplet["head"]
+            hypo2premise["relation"] = triplet["relation"]
+            hypos2premises.append(hypo2premise)
+
+    out_path = path.replace("txt", "json")
+    """json.dump(
+    #    hypos2premises,
+        open(out_path, "w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
+    print("Save {} hypothesis and premises to {}".format(len(hypos2premises), out_path))"""
+    return hypos2premises
+
 def main():
     """path = "./data/WN18RR/test.txt"
     hypos2premises = generate_nli_data(path)
     print(pd.DataFrame(hypos2premises))"""
 
-    generate_nli_data(args.train_path)
-    generate_nli_data(args.test_path)
-    generate_nli_data(args.valid_path)
+    generate_nli_data(args.train_path, args.task)
+    generate_nli_data(args.test_path, args.task)
+    generate_nli_data(args.valid_path, args.task)
 
 
 if __name__ == "__main__":
