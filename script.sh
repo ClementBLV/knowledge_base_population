8e478f1a8254ab9e08c8a908b24c5958ad98151e
ClementBLV <clement.beliveau@imt-atlantique.net>
2024-06-02 15:44:04 +0200

    Feature : add file arguments in the parser

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 BASE=$(pwd)
-ROOT=$BASE"/data/WN18RR/"
-TRAIN=$ROOT"source/train"
-TEST=$ROOT"source/test"
-VALID=$ROOT"source/valid"
+
 
 # whoch one to do
 TRAIN_BOOL=true
@@ -11,69 +8,89 @@ TEST_BOOL=true
 VALID_BOOL=true
 
 # Parse command-line arguments
+declare -A args=(
+    [--splits]=SPLIT_VALUES
+    [--both]=BOTH
+    [--bias]=BIAS
+    [--task]=TASK
+    [--processed_data_directory]=P_FILE
+)
+
 while [ $# -gt 0 ]; do
     key="$1"
-
-    case $key in
-        --splits)
-        SPLIT_VALUES="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        --both)
-        BOTH="$2"
-        shift # past argument
-        shift # past value
-        ;;
-		--bias)
-        BIAS="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        *)    # unknown option
+    if [[ -n "${args[$key]}" ]]; then
+        declare "${args[$key]}"="$2"
+        shift 2
+    else
         echo "Unknown option: $1"
         exit 1
-        ;;
-    esac
+    fi
 done
+
+
+if [[ "$TASK" == "wordnet" || "$TASK" == "wn" || "$TASK" == "wn18rr" ]]; then
+    ROOT=$BASE"/data/WN18RR/"
+else 
+    ROOT=$BASE"/data/FB15k237/"
+fi
+
+TRAIN=$ROOT"source/train"
+TEST=$ROOT"source/test"
+VALID=$ROOT"source/valid"
 
 cd src
 # preprocess the raw dataset
-python3 data_generator.py --task "wn" --train-path $TRAIN".txt" --valid-path $VALID".txt" --test-path $TEST".txt"
+python3 data_generator.py \
+            --task "$TASK" \
+            --train-path $TRAIN".txt" \
+            --valid-path $VALID".txt" \
+            --test-path $TEST".txt"
 
-echo $BIAS
+
+
 # Path 
-#P_FILE=$ROOT
-P_FILE="/users/local/c20beliv/"
 
-#--threshold_effectif \
 if $TRAIN_BOOL; then
     for SPLIT in $SPLIT_VALUES ; do 
 		# Split train
-        echo $BIAS
+
 		echo "split $SPLIT %"
-		!(python3 split.py \
-			--input_file $TRAIN".json" \
+
+		python3 split.py \
+			--input_file $TEST".json" \
 			--percentage $SPLIT \
 			--bias $BIAS\
 			--threshold_effectif "923"\
-			--output_file $P_FILE"train_"$SPLIT".json")
+			--output_file $P_FILE"train_"$SPLIT".json"
+
 		# convert to mnli format
-		!(python3 wn2mnli.py \
+		python3 wn2mnli.py \
 			--input_file $P_FILE"train_"$SPLIT".json" \
 			--output_file $P_FILE"train_"$SPLIT".mnli.json"\
-			--both $BOTH)
+			--both $BOTH \
+            --task "$TASK"""
+
 		rm -rf $P_FILE"train_"$SPLIT".json"
+        echo "tmp file $P_FILE"train_"$SPLIT".json" was successfully removed"
+
 	done
 fi
 
 # convert to NLI format
 if $TEST_BOOL; then
 	echo "******* TEST *******"
-	python3 wn2mnli.py --input_file $TEST".json" --output_file $ROOT"test.mnli.json" --both $BOTH
+	python3 wn2mnli.py \
+        --input_file $TEST".json" \
+        --output_file $ROOT"test.mnli.json" \
+        --both $BOTH \
+        --task "$TASK"""
 fi
 
 if $VALID_BOOL; then
 	echo "******* VALID *******"
-	python3 wn2mnli.py --input_file $VALID".json" --output_file $ROOT"valid.mnli.json" --both $BOTH
+	python3 wn2mnli.py \
+        --input_file $VALID".json" \
+        --output_file $ROOT"valid.mnli.json" \
+        --both $BOTH \
+        --task "$TASK"""
 fi
