Bug Fix : issues with the computation of the meta prob

src/base/compute_meta_probabilities.py
@@ -33,12 +33,12 @@ def compute_meta_probabilities(
         meta_proba_file: str, 
         logger: Logger,
         use_meta_dummy : bool,
-        voting_strategy : str
+        voting_strategy : str="max_row",
+        use_meta_global=False
     )-> List[MetaPredictionInputFeatures]: 
-    
     # Load the meta model
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    meta_model = load_meta_model(config_meta, device=device, logger=logger, use_meta_dumy=use_meta_dummy, voting_strategy=voting_strategy)
+    meta_model = load_meta_model(config_meta, device=device, logger=logger, use_meta_dumy=use_meta_dummy, voting_strategy=voting_strategy, use_meta_global=use_meta_global)
     
     for data_item in aggregated_prob:
         # Convert fused probabilities to a tensor

src/meta/meta_models.py
@@ -22,10 +22,10 @@ class VotingModel(nn.Module):
         x = x.view(batch_size, self.num_models, self.num_classes)  # Reshape to (batch_size, num_models, num_classes)
 
         if self.strategy == "average":
-            x = row_means(x).t()[0]  # Average probabilities per class
+            t = row_means(x).t()[0]  # Average probabilities per class
             if edge_case(x):
-              x = max_row(x).t()[0]
-            votes = one_hot_max(x)
+              t = max_row(x).t()[0]
+            votes = one_hot_max(t)
 
         elif self.strategy == "max_row":
             x = max_row(x).t()[0]  # Get max probability per row